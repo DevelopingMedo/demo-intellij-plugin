5dcb075384bb4de31b0e77f7d265ec4288ec2dfd
GitHub Action <action@github.com>
2023-05-27 21:55:36 +0000

    Template cleanup

--- src/main/kotlin/com/github/developingmedo/demointellijplugin/MyBundle.kt
+++ src/main/kotlin/com/github/developingmedo/demointellijplugin/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.developingmedo.demointellijplugin
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/developingmedo/demointellijplugin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/developingmedo/demointellijplugin/services/MyProjectService.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.services
+package com.github.developingmedo.demointellijplugin.services
 
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.developingmedo.demointellijplugin.MyBundle
 
 @Service(Service.Level.PROJECT)
 class MyProjectService(project: Project) {
